744597d979c3cf75c1dc295289e3afef3c95f426
luozhiyong02 <luozhiyong02@baidu.com>
2017-10-17 21:51:11 +0800

    proxy解决循环引用的原因

--- YYKit/Utility/YYWeakProxy.h
+++ YYKit/Utility/YYWeakProxy.h
@@ -14,6 +14,16 @@
 NS_ASSUME_NONNULL_BEGIN
 
 /**
+ 使用proxy避免循环引用
+ 问题：
+ 计时器常见的内存泄露原因如下，因为初始化NSTimer时指定了触发事件为self,所以说self被NSTimer强引用了，而NSTimer对象又被加
+ 入了当前的RunLoop，出现了循环引用
+ 解决思路：
+ 1 runtime解决
+ 指定target为一个单独的对象，利用runTime在target对象中动态的创建SEL方法，然后target对象关联当前视图self,当target对象执行SEL方法
+ 时，取出关联对象self，然后让self执行该方法；
+ 2 proxy解决
+ proxy持有当前试图self,将target指定为proxy,方法会转发给self,销毁时，timer不直接持有self,可以避免循环引用
  A proxy used to hold a weak object.
  It can be used to avoid retain cycles, such as the target in NSTimer or CADisplayLink.
  
